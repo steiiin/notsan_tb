4245eb31bc7a37183dbd0aab9a8f8c95cef84eb5
dev@steiiin.de <dev@steiiin.de>
2024-11-28 13:08:15 +0100

    fix: remove unsupported treatment-prop

--- lib/data/medications.dart
+++ lib/data/medications.dart
@@ -1,10 +1,8 @@
 import 'package:notsan_tb/models/content_medication.dart';
-import 'package:notsan_tb/data/treatments.dart' as treatments;
 import 'package:notsan_tb/pages/content/medications/medication_ass/import_ass.dart';
 
 const acetylsalicyl = MedicationContent(
-  name: 'Acetylsalicylsäure', treatment: treatments.acs,
-  altnames: [ 'ASS', 'Aspirin', 'Aspisol' ],
+  name: 'Acetylsalicylsäure', altnames: [ 'ASS', 'Aspirin', 'Aspisol' ],
   contentPage: MedAcetylsalicylPage()
 );
 
